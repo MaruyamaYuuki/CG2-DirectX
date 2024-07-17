13626cbc2192d47ba425f41250976c47892eb9a7
MaruyamaYuuki <k023g0106@g.neec.ac.jp>
2024-07-17 16:11:44 +0900

    読み込むobjファイルの変更

--- main.cpp
+++ main.cpp
@@ -1125,7 +1125,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	}*/
 
 	// モデル読み込み
-	ModelData modelData = LoadObjFile("resources", "plane.obj");
+	ModelData modelData = LoadObjFile("resources", "axis.obj");
 	// 頂点リソースを作る
 	ID3D12Resource* vertexResource = CreateBufferResource(device, sizeof(VertexData) * modelData.vertices.size());
 	// 頂点バッファビューを作成する
